c13bf894c22d2f29d969fb8c47bfd2ea0dcc9fc3
Rafal <rafaljaneczek@o2.pl>
2020-03-01 21:58:12 +0100

    added RoleRepository

---
+++ src/main/java/com/CarRental/CarRental/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.CarRental.CarRental.repository;
+
+import com.CarRental.CarRental.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Role findByRole(String role);
+
+}
