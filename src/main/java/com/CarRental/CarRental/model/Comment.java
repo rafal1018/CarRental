f70c61016186f46518d1bff08fadd7d9bbdcb4cc
Rafał <rafaljaneczek@o2.pl>
2020-11-11 22:52:01 +0100

    comment entity
    Signed-off-by: Rafał <rafaljaneczek@o2.pl>

---
+++ src/main/java/com/CarRental/CarRental/model/Comment.java
@@ -0,0 +1,42 @@
+package com.CarRental.CarRental.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.Date;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @Column(name = "comment_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created;
+
+    @Column(name = "message")
+    @Length(min = 5, message = "*Message must have at least 5 characters")
+    @NotEmpty(message = "*Please provide message")
+    private String message;
+
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH})
+    private User creator;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH})
+    private Car car;
+
+}
